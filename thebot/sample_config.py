d85b3970fbcc68689032f14f02aa5e1715414c57
nekozu <81870902+nekozu@users.noreply.github.com>
2021-06-05 16:12:56 +0700

    Update sample_config.py

--- thebot/sample_config.py
+++ thebot/sample_config.py
@@ -1,10 +1,10 @@
 class Config(object):
     LOGGER = True
-    API_ID = 69
-    API_HASH = "owo"
-    TOKEN = "uwu"
-    DB_URI = ""
-    LOG_CHANNEL = 0 # if you want a logging channel you can add this, else logs will go into Owner's PM
+    API_ID = 2374504
+    API_HASH = "2ea965cd0674f1663ec291313edcd333"
+    TOKEN = "1710587682:AAFk3WOqWNZ4aqOSxclvc4ZqVhIW7pjgZnE"
+    DB_URI = "postgres://ulfcpxkc:zqBMxEiu7VfwdMjnB2qpZEKySNsnBuhc@arjuna.db.elephantsql.com/ulfcpxkc"
+    LOG_CHANNEL = -1001315521755 # if you want a logging channel you can add this, else logs will go into Owner's PM
 
 class Development(Config):
     TEST_DEVELOP = True
